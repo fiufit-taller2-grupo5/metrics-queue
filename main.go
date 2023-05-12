a425697159fd7fa5dfe41f4a015d44e9f1f9ed81
Tomas Szwarcberg <tomas.szwarcberg@innovid.com>
2023-05-12 17:57:14 -0300

    Now system stops running if redis connection first fails

--- main.go
+++ main.go
@@ -1,26 +1,45 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"metrics-queue/api/controllers"
 	"metrics-queue/redis_client"
+	"os"
 )
 
-const Port = 8004
-
 func main() {
+	port := portFromEnv()
 	router := gin.Default()
-	initControllers(router)
-	err := router.Run(fmt.Sprintf(":%d", Port))
+	err := initControllers(router)
 	if err != nil {
-		fmt.Println("Error running metrics server: " + err.Error())
+		fmt.Println("Failed initializing controllers: " + err.Error())
+		return
 	}
 
+	err = router.Run(fmt.Sprintf(":%d", port))
+	if err != nil {
+		fmt.Println("Error running metrics server: " + err.Error())
+		return
+	}
 }
 
-func initControllers(router gin.IRouter) {
+func initControllers(router gin.IRouter) error {
 	redisClient := redis_client.NewRedisClient()
+	if redisClient == nil {
+		return errors.New("failed creating redis client")
+	}
+
 	metricsController := controllers.NewMetricsController(redisClient)
 	metricsController.SetUp(router)
+	return nil
+}
+
+func portFromEnv() int {
+	if os.Getenv("environment") == "production" {
+		return 80
+	} else {
+		return 8004
+	}
 }
